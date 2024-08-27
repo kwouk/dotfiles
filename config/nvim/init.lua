7793256816610204f3057d2b8107ebbdcd82bdcd
Kris Wouk <kris@wouk.me>
2024-08-27 17:58:28 -0400

    Added Lazygit integration to Neovim config.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -354,6 +354,26 @@ require("lazy").setup({
 			-- configuration here or empty for defaults
 		},
 	},
+	{
+
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
 
 	-- LSP Plugins
 	{
