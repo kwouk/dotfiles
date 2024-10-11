541f1c5d2cd6241fdd431d0f1f95e900609d7f1a
Kris Wouk <kris@wouk.me>
2024-10-11 13:27:23 -0400

    Added wezterm config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -106,8 +106,8 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "j", "gj", { desc = "Move down across wrapped lines" })
-vim.keymap.set("n", "k", "gk", { desc = "Move up across wrapped lines" })
+vim.keymap.set("n", "j", "gj", { silent = true })
+vim.keymap.set("n", "k", "gk", { silent = true })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
