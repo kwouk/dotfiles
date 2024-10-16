7541c4ee3d31d434ce76883393be115beef1ae4c
Kris Wouk <kris@wouk.me>
2024-10-16 15:52:49 -0400

    Tweaked Wezterm config slightly

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,6 +2,12 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.font = wezterm.font("Hack Nerd Font")
+
 config.color_scheme = "Tokyo Night"
+
+config.audible_bell = "Disabled"
+
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
