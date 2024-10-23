b586982755efad8d79761e32f018272b75f2f15e
Kris Wouk <kris@wouk.me>
2024-10-23 14:34:18 -0400

    tweaked font

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Hack Nerd Font")
+config.font = wezterm.font("Iosevka Nerd Font")
+config.font_size = 13
 
 config.color_scheme = "Tokyo Night"
 
